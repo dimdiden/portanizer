d6a7048af46053f43d08260e244135d64ad0550f
dimdiden <dimdiden@gmail.com>
2017-05-15 11:30:45 +0300

    Added Tag model

--- posts/models.py
+++ posts/models.py
@@ -9,6 +9,14 @@ The default HTML widget to use when rendering a form field (e.g. <input type="te
 class Post(models.Model):
     title = models.CharField(max_length=50)
     body = models.TextField()
+    tag = models.ManyToManyField('Tag')
 
     def __str__(self):
         return self.title
+
+
+class Tag(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
