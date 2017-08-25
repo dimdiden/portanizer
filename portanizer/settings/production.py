7acbd7f860d678c74e347303abc6e418767d3855
dimdiden <dimdiden@gmail.com>
2017-08-25 15:42:11 +0300

    Added secret logic for settings

--- portanizer/settings/production.py
+++ portanizer/settings/production.py
@@ -1,3 +1,14 @@
 from .base import *
 
 # DEBUG = True
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'portanizer',
+        'USER': 'ded',
+        'PASSWORD': get_secret("PASSWORD"),
+        'HOST': 'localhost',
+        'PORT': '',
+    }
+}
