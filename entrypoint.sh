0da031a66023f809c86a698890461e506f117a65
dmytro_didenko <dimdiden@gmail.com>
2019-11-21 17:04:00 +0200

    Fix syntax in entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -12,7 +12,7 @@ then
 fi
 
 # python manage.py flush --no-input
-python3 manage.py makemigrations
+python manage.py makemigrations
 python manage.py migrate
 # python manage.py collectstatic --no-input --clear
 
