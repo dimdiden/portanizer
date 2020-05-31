69fa28f7889534555a6b8b3856bed080d6284cc0
dmytro_didenko <dimdiden@gmail.com>
2020-05-31 12:46:17 +0300

    Added kubelet image build scripts

---
+++ cicd/kubelet-image.groovy
@@ -0,0 +1,48 @@
+#!/usr/bin/env groovy
+
+pipeline {
+    agent {
+        kubernetes {
+            label 'kube-slave-docker'
+            defaultContainer 'jnlp'
+            slaveConnectTimeout 200
+            yamlFile 'cicd/pod-docker.yml'
+        }
+    }
+
+    options {
+        ansiColor('xterm')
+        // parallelsAlwaysFailFast()
+        timestamps()
+        skipDefaultCheckout()
+        buildDiscarder(logRotator(numToKeepStr: '10'))
+    }
+
+    environment {
+        REGISTRY = "dimdiden/kubelet-arm"
+        DOCKER_HUB_CREDS = 'docker-hub-connector'
+    }
+
+    stages {
+        stage('Checkout') {
+            steps {
+                git 'https://github.com/dimdiden/portanizer.git'
+            }
+        }
+        stage('Build-Push') {
+            steps {
+                container('docker') {
+                    script {
+                        dir('cicd') {
+                            def dockerImage = docker.build "${env.REGISTRY}"
+
+                            docker.withRegistry('', env.DOCKER_HUB_CREDS) {
+                                dockerImage.push()
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
